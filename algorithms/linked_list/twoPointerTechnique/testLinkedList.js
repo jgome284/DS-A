e945ffbafe508a5a78dd355390b65e109afd388e
Gomez, Jose M <jose.m.gomez@lmco.com>
2024-07-10 12:38:43 -0400

    init test linked list

---
+++ algorithms/linked_list/twoPointerTechnique/testLinkedList.js
@@ -0,0 +1,12 @@
+const LinkedList = require('../../../data_structures/LinkedList.js');
+
+const testLinkedList = new LinkedList()
+testLinkedList.addToHead(1)
+testLinkedList.addToTail(2)
+testLinkedList.addToTail(3)
+testLinkedList.addToTail(4)
+testLinkedList.addToTail(5)
+testLinkedList.addToTail(6)
+testLinkedList.addToTail(7)
+
+module.exports = testLinkedList;
