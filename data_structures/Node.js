e0e48225db75be21730dc7fc47b15996e2089869
Gomez, Jose M <jose.m.gomez@lmco.com>
2024-07-12 11:01:28 -0400

    update node data structure to allow previous node linking for doubly linked lists

--- data_structures/Node.js
+++ data_structures/Node.js
@@ -1,29 +1,43 @@
 class Node {
-  // This class represents a node in a linked list
-    constructor(data) {
-        // Initialize the data property with the input data
-        this.data = data;
-        // and the next property as null
-        this.next = null;
-    }
+  // This class represents a node in a linked or doubly linked list
+  constructor(data) {
+    // Initialize the data property with the input data
+    this.data = data;
+    // and the next property as null
+    this.next = null;
+    // set previous link property as null
+    this.previous = null;
+  }
 
-    // Method to set the next node in the linked list
-    setNextNode(node) {
-        // Check if the input node is an instance of the Node class or is null
-        if (node instanceof Node || node === null) {
-            // If so, set this node's next property to the input node
-            this.next = node;
-        } else {
-            // Otherwise, throw an error indicating that the next node must be a Node instance
-            throw new Error("Next node must be a member of the Node class.");
-        }
+  // Method to set the next node in the linked list
+  setNextNode(node) {
+    // Check if the input node is an instance of the Node class or is null
+    if (node instanceof Node || node === null) {
+      // If so, set this node's next property to the input node
+      this.next = node;
+    } else {
+      // Otherwise, throw an error indicating that the next node must be a Node instance
+      throw new Error('Next node must be a member of the Node class.');
     }
+  }
 
-    // Method to get the next node in the linked list
-    getNextNode() {
-        // Return the value of this node's next property
-        return this.next;
+  setPreviousNode(node) {
+    if (node instanceof Node || node === null) {
+      this.previous = node;
+    } else {
+      throw new Error('Previous node must be a member of the Node class');
     }
+  }
+
+  // Method to get the next node in the linked list
+  getNextNode() {
+    // Return the value of this node's next property
+    return this.next;
+  }
+  // Method to get the previous node in a doubly linked list
+  getPreviousNode() {
+    return this.previous;
+  }
 }
 
 module.exports = Node;
