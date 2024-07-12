151c5a5a0bc48f95faca001c02e28592a377597d
Gomez, Jose M <jose.m.gomez@lmco.com>
2024-07-12 12:56:06 -0400

    add runway queue example

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Node } = require('./data_structures/Node.js');
 const LinkedList = require('./data_structures/LinkedList.js');
 const DoublyLinkedList = require('./data_structures/DoublyLinkedList.js');
+const Queue = require('./data_structures/Queue.js');
 
 // Example of Nodes
 console.log(
@@ -69,7 +70,47 @@ subway.removeTail();
 subway.printList();
 
 console.log(
-  '\nTimes Square station is under construction...Updating ROute...\n'
+  '\nTimes Square station is under construction...Updating Route...\n'
 );
 subway.removeByData('TimesSquare');
 subway.printList();
+
+console.log(
+  '\nQueue Example: \nWe can model flights waiting their turn at a runway with a max of three waiting at a given time...\n'
+);
+
+const flights = [
+  'Botswana Bird flight #345',
+  'Singapore Skies flight #890',
+  'Mexico Mirage flight #234',
+  'Greenland Flying Seals flight #567',
+];
+
+const load = (flights) => {
+  const runway = new Queue(3);
+
+  flights.forEach((flight) => {
+    try {
+      runway.enqueue(flight);
+      console.log(`${flight} taxi to runway.`);
+    } catch (error) {
+      console.log('Runway full!');
+    }
+  });
+  return runway;
+};
+
+const clear = (runway) => {
+  while (!runway.isEmpty()) {
+    const cleared = runway.dequeue();
+    console.log('\nFlights wait...\n');
+    console.log(`${cleared}, is cleared for takeoff!\n${cleared} in air.`);
+  }
+
+  console.log('\nAll planes took off, runway clear.');
+};
+
+// Enqueue runway with planes
+const runway = load(flights);
+// Clear each plane to takeoff
+clear(runway);
